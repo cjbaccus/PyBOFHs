308496a8042343df48e6a686f6939b6c9867b538
Carl Baccus <cjbaccus@gmail.com>
2015-09-06 19:38:35 -0700

    Final working BOFH motd generator

--- motd.py
+++ motd.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 
-bofhs = open("bofh.txt")
-bline = bofhs.read()
-print bline
+# abstract: take contents from bofh.txt and randomly choose one line to print out
+# this script will be used in crontab to randomly generate message of the day lines from BOFH
+# This script replaces a perl script written by Joshua Beining... Thanks and Kudos to Joshua for
+# inspiring this
+# Author: Carl Baccus 2015
+
+import os, random
+line = random.choice(open('bofh.txt').readlines())
+print line
